c539ffc4f77a53ebaad6d6595e1aa4cb45954db3
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2022-05-27 06:15:52 +0200

    moving pawns on the board possible, communication with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ export class AppComponent implements OnInit {
   currentState: any;
   currentMove: any = "w"; //to be empty
   selectedPawnId: any;
-  selectedPawnColour: any;
   moveTo: any;
 
   properties01 :any = "r";
@@ -75,6 +74,8 @@ export class AppComponent implements OnInit {
     if (this.moveTo != null && this.selectedPawnId != null) {
 
       this.currentState =
+        this.currentMove +
+        "/" +
         this.properties01 +
         this.properties03 +
         this.properties05 +
@@ -110,14 +111,53 @@ export class AppComponent implements OnInit {
 
      let move = this.selectedPawnId.toString() + "->" + this.moveTo.toString();
 
+
+
+
       //send request to the front and receive new state
       this.checkersClientService.getBoardState(this.currentState, move).subscribe(value => {
         this.currentState = value;
-      })
 
+        this.currentMove = (<string><unknown>value).split("/")[0]
+        let newBoardState = (<string><unknown>value).split("/")[1].split("")
+
+        this.properties01 = newBoardState[0]
+        this.properties03 = newBoardState[1]
+        this.properties05 = newBoardState[2]
+        this.properties07 = newBoardState[3]
+        this.properties10 = newBoardState[4]
+        this.properties12 = newBoardState[5]
+        this.properties14 = newBoardState[6]
+        this.properties16 = newBoardState[7]
+        this.properties21 = newBoardState[8]
+        this.properties23 = newBoardState[9]
+        this.properties25 = newBoardState[10]
+        this.properties27 = newBoardState[11]
+        this.properties30 = newBoardState[12]
+        this.properties32 = newBoardState[13]
+        this.properties34 = newBoardState[14]
+        this.properties36 = newBoardState[15]
+        this.properties41 = newBoardState[16]
+        this.properties43 = newBoardState[17]
+        this.properties45 = newBoardState[18]
+        this.properties47 = newBoardState[19]
+        this.properties50 = newBoardState[20]
+        this.properties52 = newBoardState[21]
+        this.properties54 = newBoardState[22]
+        this.properties56 = newBoardState[23]
+        this.properties61 = newBoardState[24]
+        this.properties63 = newBoardState[25]
+        this.properties65 = newBoardState[26]
+        this.properties67 = newBoardState[27]
+        this.properties70 = newBoardState[28]
+        this.properties72 = newBoardState[29]
+        this.properties74 = newBoardState[30]
+        this.properties76 = newBoardState[31]
+      })
 
       this.moveTo = null
       this.selectedPawnId = null
+
     }
 
 
