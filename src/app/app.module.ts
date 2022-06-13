51a57bd3aedcb004a30066e87ed998e96056e28a
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2022-06-13 07:14:56 +0200

    second board rotated by 180dgr added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BoardForWhiteComponent } from './board-for-white/board-for-white.component';
+import { BoardForRedComponent } from './board-for-red/board-for-red.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardForWhiteComponent,
+    BoardForRedComponent
   ],
   imports: [
     BrowserModule,
