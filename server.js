0841da07a6888d16de394cf122c723a58e336dca
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2022-07-14 07:12:33 +0200

    adjustment to heroku deploy

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ res.sendFile(path.join(__dirname+'/dist/checkersWithAngular/index.html'));
 
 // Start the app by listening on the default Heroku port
 //BACKEND ON SERVER (PORT 8080) OR FROM LOCALHOST (PORT 4200)
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
