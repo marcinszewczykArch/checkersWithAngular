04d10cc37ba21e63215b0dca1ec213a584be4ea0
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2022-07-14 07:50:09 +0200

    adjustment to heroku deploy

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/'));
+app.use(express.static(__dirname + '/dist/<app-name>'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
-});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/<app-name>/index.html'));});
 
 // Start the app by listening on the default Heroku port
 //BACKEND ON SERVER (PORT 8080) OR FROM LOCALHOST (PORT 4200)
